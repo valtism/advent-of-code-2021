dbd26d30c1be0b6ac8438ef21a5c5c59fbed18e2
Dan Wood <daniel4wood@icloud.com>
2021-12-02 16:19:11 +1100

    Finished day 2

--- README.md
+++ README.md
@@ -11,8 +11,8 @@
 
 <!--SOLUTIONS-->
 
-![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)
-![Day](https://badgen.net/badge/02/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
+[![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
 ![Day](https://badgen.net/badge/03/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
@@ -69,16 +69,16 @@ npm start 1
 
 ```
 Day 01
-Time part 1: - 00:04:24
-Time part 2: - 00:04:09
-Both parts: - 00:08:33
+Time part 1: 0.51ms
+Time part 2: 0.28ms
+Both parts: 0.79ms
 ```
 
 ```
 Day 02
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 0.93ms
+Time part 2: 0.56ms
+Both parts: 1.4900000000000002ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 0/50
-Total time: 0ms
+Total stars: 4/50
+Total time: 2.2800000000000002ms
 ```
 
 <!--/RESULTS-->
---
+++ dist/day02/index.js
@@ -0,0 +1,72 @@
+import run from "aocrunner";
+const parseInput = (rawInput) => rawInput;
+const part1 = (rawInput) => {
+  const input = parseInput(rawInput);
+  const rows = input.split("\n").map((x) => {
+    const elements = x.split(" ");
+    return { instruction: elements[0], value: Number(elements[1]) };
+  });
+  const forwards = rows.filter((x) => x.instruction === "forward").reduce((acc, curr) => acc + curr.value, 0);
+  const up = rows.filter((x) => x.instruction === "up").reduce((acc, curr) => acc + curr.value, 0);
+  const down = rows.filter((x) => x.instruction === "down").reduce((acc, curr) => acc + curr.value, 0);
+  return forwards * (down - up);
+};
+const part2 = (rawInput) => {
+  const input = parseInput(rawInput);
+  const rows = input.split("\n").map((x) => {
+    const elements = x.split(" ");
+    return { instruction: elements[0], value: Number(elements[1]) };
+  });
+  let aim = 0;
+  let depth = 0;
+  let position = 0;
+  for (const row of rows) {
+    switch (row.instruction) {
+      case "down":
+        aim += row.value;
+        break;
+      case "up":
+        aim -= row.value;
+        break;
+      case "forward":
+        position += row.value;
+        depth += aim * row.value;
+        break;
+      default:
+        break;
+    }
+  }
+  return depth * position;
+};
+run({
+  part1: {
+    tests: [
+      {
+        input: `forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2`,
+        expected: 150
+      }
+    ],
+    solution: part1
+  },
+  part2: {
+    tests: [
+      {
+        input: `forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2`,
+        expected: 900
+      }
+    ],
+    solution: part2
+  },
+  trimTestInputs: true
+});
+//# sourceMappingURL=index.js.map
