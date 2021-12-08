ba9f9c1ad3546d5548e75bae922336ef28df4096
Dan Wood <daniel4wood@icloud.com>
2021-12-08 18:14:06 +1100

    Day 8 done.

---
+++ dist/day08/index.js
@@ -0,0 +1,97 @@
+import run from "aocrunner";
+const parseInput = (rawInput) => rawInput.split("\n").map((row) => row.split(" | ").map((row2) => row2.split(" ")));
+const part1 = (rawInput) => {
+  const input = parseInput(rawInput);
+  let count = 0;
+  for (const row of input) {
+    count += row[1].filter((row2) => [2, 3, 4, 7].includes(row2.length)).length;
+  }
+  return count;
+};
+const part2 = (rawInput) => {
+  const input = parseInput(rawInput);
+  let sum = 0;
+  for (const row of input) {
+    const leftSorted = row[0].map((r) => r.split("").sort().join("")).sort();
+    const match = permutations.find((permutation) => {
+      const codes = positionsNumbers.map((nums2) => nums2.map((num) => permutation[num]).sort().join("")).sort();
+      return codes.every((code, i) => code === leftSorted[i]);
+    });
+    const nums = row[1].map((code) => code.split("").map((c) => match.indexOf(c)).sort((a, b) => a - b).toString()).map((numArr) => positionsNumbers.findIndex((value, i) => numArr === value.toString()));
+    sum += Number(nums.join(""));
+  }
+  return sum;
+};
+function permutator(inputArr) {
+  let result = [];
+  const permute = (arr, m = []) => {
+    if (arr.length === 0) {
+      result.push(m);
+    } else {
+      for (let i = 0; i < arr.length; i++) {
+        let curr = arr.slice();
+        let next = curr.splice(i, 1);
+        permute(curr.slice(), m.concat(next));
+      }
+    }
+  };
+  permute(inputArr);
+  return result;
+}
+const positionsNumbers = [
+  [0, 1, 2, 4, 5, 6],
+  [2, 5],
+  [0, 2, 3, 4, 6],
+  [0, 2, 3, 5, 6],
+  [1, 2, 3, 5],
+  [0, 1, 3, 5, 6],
+  [0, 1, 3, 4, 5, 6],
+  [0, 2, 5],
+  [0, 1, 2, 3, 4, 5, 6],
+  [0, 1, 2, 3, 5, 6]
+];
+const permutations = permutator(["a", "b", "c", "d", "e", "f", "g"]);
+run({
+  part1: {
+    tests: [
+      {
+        input: `be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce`,
+        expected: 26
+      }
+    ],
+    solution: part1
+  },
+  part2: {
+    tests: [
+      {
+        input: `acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf`,
+        expected: 5353
+      },
+      {
+        input: `be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce`,
+        expected: 61229
+      }
+    ],
+    solution: part2
+  },
+  trimTestInputs: true
+});
+//# sourceMappingURL=index.js.map
---
+++ src/day08/README.md
@@ -0,0 +1,9 @@
+# 🎄 Advent of Code 2021 - day 8 🎄
+
+## Info
+
+Task description: [link](https://adventofcode.com/2021/day/8)
+
+## Notes
+
+Spent 20 minutes trying to fix a bug when my test input was wrong 🤦
